6e53d6e19e9e10e7fb651640ff2e4c895e1d3d43
mutuam <lodekib@gmail.com>
2023-08-26 16:34:56 +0300

    initial commit

---
+++ index.js
@@ -0,0 +1,42 @@
+let navbar = document.querySelectorAll('.navbar');
+document.querySelector('#login-btn').onclick = () =>{
+   navbar.classList.toggle('active');
+   loginForm.classList.remove('active');
+}
+
+
+
+
+let loginForm = document.querySelector('.login-form');
+
+document.querySelector('#login-btn').onclick = () =>{
+   loginForm.classList.toggle('active');
+   navbar.classList.remove('active');
+}
+
+window.onscroll = () =>{
+   navbar.classList.toggle('active');
+   loginForm.classList.remove('active');
+}
+
+var swiper = new Swiper(".review-slider",{
+ spaceBetween:20,
+   centeredSlides:true,
+   grabCursor:true,
+  autoplay:{
+      delay:7500,
+      disableOnInteraction:false,
+   },
+   loop:true,
+   breakpoints:{
+     0: {
+         slidesPerView: 1,
+      },
+      768:{
+         slidesPerView: 2,
+      },
+      991:{
+         slidesPerView: 3,
+      },
+   },
+});
